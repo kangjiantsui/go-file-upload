6a921c6244bd80cfc33d96dc1cbf16a5197c9c5c
kangjian <690656728@qq.com>
2024-04-15 19:10:57 +0800

    docker hub镜像上传,compose使用docker hub的镜像

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,25 @@
-GOOS=linux GOARCH=amd64 go build -o build/go-file-upload cmd/main.go
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+# 设置编译环境变量
+export GOOS=linux
+export GOARCH=amd64
 
+# 构建 Go 项目
+go build -o build/go-file-upload cmd/main.go
+
+# 获取当前时间，格式为：年月日时分，例如 202404151235
+TIMESTAMP=$(date +"%Y%m%d%H%M")
+
+# 构建 Docker 镜像
 docker build -t image-service -f Dockerfile .
-docker rm -f image-service
-docker run -d --name image-service -p 5408:5408 image-service
+
+# 使用时间戳标记镜像
+docker tag image-service:latest "kangjiantsui/image-service:$TIMESTAMP"
+
+# 同时更新 latest 标签
+docker tag image-service:latest kangjiantsui/image-service:latest
+
+# 推送镜像到 Docker Hub
+docker push "kangjiantsui/image-service:$TIMESTAMP"
+docker push kangjiantsui/image-service:latest
+
